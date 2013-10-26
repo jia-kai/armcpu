fba09ca5e28f01db38aac5bacb6e5885442929c5
jiakai <jia.kai66@gmail.com>
2013-10-26 20:53:55 +0800

    serial port @115200 finally works

--- comtest/comtest.v
+++ comtest/comtest.v
@@ -1,6 +1,6 @@
 /*
  * $File: comtest.v
- * $Date: Sat Oct 26 20:04:47 2013 +0800
+ * $Date: Sat Oct 26 20:45:53 2013 +0800
  * $Author: jiakai <jia.kai66@gmail.com>
  */
 
@@ -14,8 +14,8 @@ module comtest(
 	output baseram_oe,
 	output baseram_ce,
 	output baseram_we,
-	output uart_enable_recv,
-	output uart_TxD_start,
+	output reg uart_enable_recv,
+	output reg uart_TxD_start,
 	input uart_TxD_busy,
 	input uart_RxD_data_ready,
 	input uart_RxD_waiting_data,
@@ -26,120 +26,53 @@ module comtest(
 	assign baseram_we = 1;
 	assign uart_rst = ~rst;
 
-	assign uart_TxD_start = 0;
-	assign uart_enable_recv = 1;
-
-	reg [7:0] frame_num;
-	reg [7:0] prev_recv_data;
-
-	assign led = {prev_recv_data,
-		5'b0, uart_RxD_data_ready, uart_RxD_waiting_data};
-
-	always @(posedge clk) begin
-		if (~rst) begin
-			frame_num <= 0;
-			prev_recv_data <= 0;
-		end else
-			if (uart_RxD_data_ready) begin
-				prev_recv_data <= baseram_data;
-				frame_num <= frame_num + 1'b1;
-			end
-	end
-
-	digseg_driver data_disp_high(
-		.data(frame_num[7:4]),
-		.seg(segdisp1));
-
-	digseg_driver data_disp_low(
-		.data(frame_num[3:0]),
-		.seg(segdisp0));
-
 	/*
-	reg [7:0] frame_num_indicator;
-	reg [7:0] data_to_write;
-	reg [7:0] data_from_com_prev;
+	* receive NULL terminated string, calc xor of chars
+	*/
+
 	reg is_write;
-	reg [2:0] state;
-
-	assign led = {frame_num_indicator, 
-		state[1:0],
-		is_write, uart_tbre, uart_tsre, uart_data_ready,
-		uart_framing_error, uart_parity_error};
-
-	digseg_driver data_disp_high(
-		.data(data_from_com_prev[7:4]),
-		.seg(segdisp1));
-
-	digseg_driver data_disp_low(
-		.data(data_from_com_prev[3:0]),
-		.seg(segdisp0));
-	
-
-
+	reg [7:0] data_to_write;
 	assign baseram_data = is_write ? data_to_write : {8{1'bz}};
 
+	reg [7:0] frame_num_shift;
+	assign led = {frame_num_shift,
+		4'b0, is_write, uart_TxD_busy, uart_RxD_data_ready, uart_RxD_waiting_data};
 
+	digseg_driver disp_data_high(.data(data_to_write[7:4]), .seg(segdisp1));
+	digseg_driver disp_data_low(.data(data_to_write[3:0]), .seg(segdisp0));
+
+	reg state;
 	always @(posedge clk) begin
-		if (!rst) begin
-			state <= 0;
+		if (uart_rst) begin
+			data_to_write <= 0;
 			is_write <= 0;
-			frame_num_indicator <= 0;
 		end else if (is_write) begin
-			// writing to com
-			uart_rdn <= 1;
+			uart_enable_recv <= 0;
 			if (state == 0) begin
 				state <= 1;
-				uart_wrn <= 1;
+				uart_TxD_start <= 1;
 			end else if (state == 1) begin
-				uart_wrn <= 0;
-				state <= 2;
-			end else if (state == 2) begin
-				uart_wrn <= 1;
-				state <= 3;
-			end else if (state == 3) begin
-				if (uart_tbre)
-					state <= 4;
-			end else if (state == 4) begin
-				if (uart_tsre) begin
-					is_write <= 0;
+				uart_TxD_start <= 0;
+				if (~uart_TxD_busy) begin
 					state <= 0;
 					data_to_write <= 0;
+					is_write <= 0;
 				end
 			end else
 				state <= 0;
 		end else begin
-			// reading from com
-			uart_wrn <= 1;
-			if (state == 0) begin
-				state <= 1;
-			end else if (state == 1) begin
-				uart_rdn <= 1;
-				state <= 2;
-			end else if (state == 2) begin
-				if (uart_data_ready) begin
-					state <= 3;
-					uart_rdn <= 0;
-				end
-			end else if (state == 3) begin
+			uart_enable_recv <= 1;
+			if (uart_RxD_data_ready) begin
 				data_to_write <= data_to_write ^ baseram_data;
-				frame_num_indicator <= {frame_num_indicator[6:0],
-					~(|frame_num_indicator[6:0])};
-
-				if (|baseram_data) begin
-					data_from_com_prev <= baseram_data;
-					uart_rdn <= 1;
-					state <= 2;
-				end
-				else begin
-					is_write <= 1;
+				frame_num_shift <= {frame_num_shift[6:0], !frame_num_shift[6:0]};
+				if (!baseram_data) begin
 					state <= 0;
+					is_write <= 1;
 				end
-			end else
-				state <= 0;
+			end
 		end
-
 	end
-	*/
+
 
 endmodule
 
--- comtest/serial_test.py
+++ comtest/serial_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# $File: serial_writer.py
-# $Date: Sat Oct 26 16:48:07 2013 +0800
+# $File: serial_test.py
+# $Date: Sat Oct 26 20:43:11 2013 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
 
 import serial   # packge pyserial
@@ -10,7 +10,7 @@ import sys
 import random
 import operator
 
-CHUNK_SIZE = 10
+CHUNK_SIZE = 1024
 
 def gen_data():
     """:return: data, expected result"""
@@ -18,9 +18,8 @@ def gen_data():
     expect = chr(reduce(operator.xor, data))
     return ''.join(map(chr, data)) + '\x00', expect
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, # why 115200 won't work?
-        stopbits=1,
-        parity=serial.PARITY_EVEN, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', 115201,
+        stopbits=1, parity=serial.PARITY_NONE, timeout=1)
 try:
     tstart = time.time()
     cnt = 0
@@ -32,9 +31,8 @@ try:
         if rst != expect:
             nr_err += 1
         cnt += 1
-        sys.stdout.write('speed: {:.2f} bytes/sec; err={} tot={} d={} rst={}\r'.format(
-                cnt * CHUNK_SIZE / (time.time() - tstart), nr_err, cnt,
-                data.encode('hex'), rst.encode('hex')))
+        sys.stdout.write('speed: {:.2f} bytes/sec; err={} tot={}\r'.format(
+                cnt * CHUNK_SIZE / (time.time() - tstart), nr_err, cnt))
         sys.stdout.flush()
 except Exception as e:
     print e
