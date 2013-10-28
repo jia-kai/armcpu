ff82da55fd6803d3264e7056286178ab93903f1c
jiakai <jia.kai66@gmail.com>
2013-10-28 22:44:24 +0800

    driver seems to work...

--- lib/flash_driver.v
+++ lib/flash_driver.v
@@ -1,6 +1,6 @@
 /*
  * $File: flash_driver.v
- * $Date: Mon Oct 28 09:15:12 2013 +0800
+ * $Date: Mon Oct 28 21:51:34 2013 +0800
  * $Author: jiakai <jia.kai66@gmail.com>
  */
 
@@ -13,30 +13,35 @@ module flash_driver
 	input [FLASH_ADDR_SIZE - 1:0] addr,
 	input [15:0] data_in,
 	output [15:0] data_out,
-	input enable_read,
 	/*
-	* when asserted, data could be continousely read from flash
-	* busy would be de-asserted for one cycle after finishing each read
-	* addr must be valid during the whole reading process
+	* when asserted, data could be continousely read from flash, just change
+	* addr and read in next cycle when busy is 0
 	*/
-	input enable_erase,
+	input enable_read,
 	// assert for one cycle to erase; addr woule be latched
-	input enable_write,
+	input enable_erase,
 	// assert for one cycle to write; addr and data woule be latched
+	input enable_write,
+	/*
+	* indicate whether can start reading in read mode
+	* indicate whether device is busy in writing and erasing mode
+	*/
 	output reg busy,
 	output [FLASH_ADDR_SIZE:0] flash_addr,
 	inout [15:0] flash_data,
 	output [7:0] flash_ctl);
 
+
+
+	assign data_out = flash_data;
+
 	reg flash_oe, flash_we;
 
 	wire flash_byte = 1, flash_vpen = 1, flash_ce = 0, flash_rp = 1;
 
-	reg [FLASH_ADDR_SIZE - 1:0] flash_addr_sig;	// significant flash addr
-	assign flash_addr = {flash_addr_sig, 1'b0};
-	reg [15:0] data_to_write,
-		data_cache;	/* for both caching output data when reading, and latching
-						input data when writing */
+	reg [FLASH_ADDR_SIZE - 1:0] addr_latch;	// significant flash addr
+	assign flash_addr = {enable_read ? addr : addr_latch, 1'b0};
+	reg [15:0] data_to_write, data_in_latch;	
 	assign flash_data = flash_oe ? data_to_write : {16{1'bz}};
 
 	assign flash_ctl = {
@@ -50,20 +55,28 @@ module flash_driver
 
 	reg [3:0] state;
 	localparam
-		IDLE = 3'b000,
-		READ1 = 3'b001, READ2 = 3'b011, READ3 = 4'b1011,
-		WRITE1 = 3'b010, WRITE2 = 4'b1110, WRITE3 = 4'b1111,
-		ERASE1 = 3'b100, ERASE2 = 4'b1100, ERASE3 = 4'b1101,
-		SR1 = 3'b101, SR2 = 3'b111, SR3 = 3'b110, SR4 = 4'b1000;
-
-	assign data_out = state == READ3 ? flash_data : data_cache;
+		IDLE = 4'b0000,
+		WRITE1 = 4'b0001,
+		WRITE2 = 4'b0011,
+		WRITE3 = 4'b0010,
+		ERASE1 = 4'b0110,
+		ERASE2 = 4'b0111,
+		ERASE3 = 4'b0101,
+		READ1 = 4'b0100,
+		READ2 = 4'b1100,
+		READ3 = 4'b1101,
+		READ4 = 4'b1111,
+		SR1 = 4'b1110,
+		SR2 = 4'b1010,
+		SR3 = 4'b1011,
+		SR4 = 4'b1001;
 
 	always @(posedge clk) begin
 		case (state)
 			IDLE: begin
-				flash_addr_sig <= addr;
+				addr_latch <= addr;
 				if (enable_write) begin
-					data_cache <= data_in;
+					data_in_latch <= data_in;
 					data_to_write <= 16'h0040;
 					flash_we <= 0;
 					state <= WRITE1;
@@ -91,7 +104,7 @@ module flash_driver
 			end
 			WRITE2: begin
 				flash_we <= 0;
-				data_to_write <= data_cache;
+				data_to_write <= data_in_latch;
 				state <= WRITE3;
 			end
 			WRITE3: begin
@@ -119,20 +132,15 @@ module flash_driver
 			end
 			READ2: begin
 				flash_oe <= 0;
-				flash_addr_sig <= addr;
 				state <= READ3;
-				busy <= 0;
 			end
 			READ3: begin
-				data_cache <= flash_data;
-				if (enable_read) begin
-					state <= READ2;
-					busy <= 1;
-				end
-				else begin
+				busy <= 0;
+				state <= READ4;
+			end
+			READ4: begin
+				if (!enable_read)
 					state <= IDLE;
-					flash_oe <= 1;
-				end
 			end
 
 			// wait for SR[7] to become 1
