03a703ecd6c64aaef188f7b3d4ecedb5568107e0
jiakai <jia.kai66@gmail.com>
2013-11-21 21:36:13 +0800

    merged test cases from Lee; but current instr set is incomplete ...

--- armcpu/simulate/prog/full_branch.s
+++ armcpu/simulate/prog/full_branch.s
@@ -3,28 +3,22 @@ li $a0, 0
 li $a1, 1
 li $a2, -1
 beq $zero, $a0, label_beq
-nop
 
 label_beq:
 	bgez $a1, label_bgez
-	nop
-
+	
 label_bgez:
 	bgtz $a1, label_bgtz
-	nop
-
+	
 label_bgtz:
 	blez $a0, label_blez
-	nop
-
+	
 label_blez:
 	bltz $a2, label_bltz
-	nop
-
+	
 label_bltz:
 	bne $zero, $a1, label_bne
-	nop
-
+	
 label_bne:
 	j halt
 
