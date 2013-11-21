f46725605b497dad7d12d73ad4a6457451edf553
jiakai <jia.kai66@gmail.com>
2013-11-21 21:41:00 +0800

    ...

--- armcpu/simulate/prog/com.py
+++ armcpu/simulate/prog/com.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # $File: com.py
-# $Date: Thu Nov 21 21:29:29 2013 +0800
+# $Date: Thu Nov 21 21:40:32 2013 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
 
 DEVICE = '/dev/ttyUSB0'
@@ -16,7 +16,14 @@ ser = serial.Serial(DEVICE, 115201,
 while True:
     raw_input()
     n = random.randint(0, 255)
-    print 'write: {} expect resp: {}'.format(hex(n), hex(n + 1))
-    ser.write(chr(n))
-    print 'got resp: {}'.format(hex(ord(ser.read())))
+    exp = (n + 1) % 255
+    print 'write: {} expect resp: {}'.format(hex(n), hex(exp))
+
+    # should only receive first
+    ser.write(chr(n) + chr(random.randint(0, 255))) 
+
+    got = ord(ser.read())
+    print 'got resp: {}'.format(hex(got))
+
+    assert got == exp
 
