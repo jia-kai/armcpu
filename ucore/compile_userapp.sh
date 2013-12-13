a8eaf2f7e961afa5f66936ebd97f0008300eed1f
jiakai <jia.kai66@gmail.com>
2013-12-13 16:16:01 +0800

    compile user app

---
+++ ucore/compile_userapp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+# $File: compile_userapp.sh
+# $Date: Fri Dec 13 16:12:53 2013 +0800
+# $Author: jiakai <jia.kai66@gmail.com>
+
+GCCPREFIX=mips-sde-elf-
+
+LD=${GCCPREFIX}ld
+CC=${GCCPREFIX}gcc
+CFLAGS="-mips1 -O2 -fno-builtin -nostdlib  -nostdinc -G0 -Wformat -EL"
+
+src=$1
+out=$2
+obj=${out}.o
+
+
+if [ -z "$out" ]
+then
+	echo "usage: $0 <.c source file> <output file>"
+	exit
+fi
+
+$CC -c $src -Iuser/libs -Ikern/include $CFLAGS -o $obj
+$LD -S -T user/libs/user.ld $obj obj/user/libuser.a -o $out
+rm -f $obj
